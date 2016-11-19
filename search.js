4dc21f0d89479b0ff02cbb02be787faef5a3f40a
Robert Washbourne <rawashbourne@gmail.com>
2016-11-19 16:11:19 -0600

    autocomplete

--- search.js
+++ search.js
@@ -2,14 +2,65 @@ document.addEventListener("DOMContentLoaded", function() {
 	chrome.storage.sync.get({"search": "google", "searchplace": "bottom"}, searchCallback);
 });
 
+var jsonmain;
+
+function jsonparse(json) {
+	jsonmain = json;
+	searchbox = document.getElementById("search-box");
+	if (json.AbstractSource == "Wikipedia") {
+		document.getElementById("duckduckres").innerHTML = "";
+		var script = document.createElement("script");
+		script.setAttribute("src", "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="+json.AbstractURL.split('/')[json.AbstractURL.split('/').length-1]+"&callback=wikicallback");
+		//script.setAttribute("src", "https://en.wikipedia.org/w/api.php?format=json&action=query&list=search&prop=extracts&exintro=&explaintext=&callback=wikicallback&srsearch="+searchbox.value);
+		document.body.appendChild(script);
+		script.outerHTML = "";
+		script.delete;
+		
+		if (typeof json.RelatedTopics[0] != "undefined") {
+			var div = document.createElement('div');
+			div.innerHTML = json.RelatedTopics[0].Result;
+			var a = div.getElementsByTagName("a")[0];
+			a.className = "extrares";
+			document.getElementById("duckduckres").appendChild(a);
+
+			div.innerHTML = json.RelatedTopics[1].Result;
+			var a = div.getElementsByTagName("a")[0];
+			a.className = "extrares";
+			document.getElementById("duckduckres").appendChild(a);
+
+			div.innerHTML = json.RelatedTopics[2].Result;
+			var a = div.getElementsByTagName("a")[0];
+			a.className = "extrares";
+			document.getElementById("duckduckres").appendChild(a);
+		}
+	}
+}
+
+function wikicallback(json) {
+	var page = json.query.pages[Object.keys(json.query.pages)[0]];
+	//console.log(json.query.pages);
+	var a = document.createElement('a');
+	a.href = "http://en.wikipedia.org/?curid="+page.pageid;
+	a.innerText = page.title + " - "+page.extract;
+	if (page.extract != "") {
+		a.innerText = page.title + " - "+page.extract;
+	} else {
+		a.innerText = page.title;
+	}
+	a.className = "mainres";
+	[].forEach.call(document.querySelectorAll('.mainres'),function(e){
+	  e.parentNode.removeChild(e);
+	});
+	document.getElementById("wikires").appendChild(a);
+}
+
 var searchCallback = function(list) {
 	var form;
 	var searchbox;
-	console.log(list);
 
 	var searchplace = list["searchplace"];
 	var formstring = "<form id=\"search-form\" method=\"get\" action=\"https://google.com/search?\">\
-			<input id=\"search-box\" name=\"q\" type=\"text\" placeholder=\"search\" autofocus/>\
+			<input id=\"search-box\" name=\"q\" type=\"text\" placeholder=\"search\" autofocus autocomplete='off'/>\
 		</form>";
 
 	if (searchplace == "top") {
@@ -25,6 +76,17 @@ var searchCallback = function(list) {
 	
 	form = document.getElementById("search-form");
 	searchbox = document.getElementById("search-box");
+
+	searchbox.onkeyup = function(){
+		var val = searchbox.value;
+		if (val.length > 1) {
+			var script = document.createElement('script');
+			script.src = 'https://api.duckduckgo.com/?q='+searchbox.value+'&format=json&callback=jsonparse';
+			document.body.appendChild(script);
+			script.outerHTML = "";
+			script.delete;
+		}
+	}
 
 	if (list["search"] == "duckduckgo") {
 		form.setAttribute("action", "https://duckduckgo.com/?");
